c7a74485d87192bcaf6c29869754749ab53381e4
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-09-23 16:39:58 +0200

    lista longitud y final

---
+++ ejercicios/ejercicio2.py
@@ -0,0 +1,18 @@
+from numpy import true_divide
+
+
+cadena = str(input())
+
+def cadena_a_lista(cadena):
+    lista = list(cadena)
+    print(lista)
+    solucion = lista_longitud(lista)
+    print(solucion)
+
+def lista_longitud(lista):
+    if 3 <= len(lista) <= 10:
+        return True
+    else:
+        return False
+
+cadena_a_lista(cadena)
