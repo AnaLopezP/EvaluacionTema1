2f4facd3d9932fc0fd54e893e8fbf9db02fa2a82
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-09-23 17:01:10 +0200

    lista 1

--- ejercicios/ejercicio3.py
+++ ejercicios/ejercicio3.py
@@ -1,3 +1,18 @@
 x = range(-20, 51)
 lista = list(x)
 print(lista)
+lista1 = []
+
+def lista_1(lista, n):
+    if n < len(lista):
+        if 0 <= lista[n] <= 10:
+                a = lista.pop(n)
+                lista1.append(a)
+                lista_1(lista,n)
+        else:
+            lista_1(lista, n+1)
+    else:
+        pass
+
+lista_1(lista, 0)
+print(lista1)
