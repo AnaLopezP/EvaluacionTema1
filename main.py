77919ab863d253d35889ffd2032108bd65478a46
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-09-23 17:30:55 +0200

    main bien hecho

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import ejercicios.ejercicio2
 import ejercicios.ejercicio3
 
 if __name__ == "__main__":
-    ejercicios.ejercicio1.separar()
+    ejercicios.ejercicio1
     ejercicios.ejercicio2
     ejercicios.ejercicio3
